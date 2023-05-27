4569aca407dc86c9e1204a39dccc1d0d56cb950a
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-05-27 18:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function updateElementWithText() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const text = 'Hello, world!';
+      resolve(text);
+    }, 1000);
+  });
+}
+
+// Function to update HTML element with resolved text
+function setTextToElement(elementId, text) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.textContent = text;
+  }
+}
+
+// Usage
+const outputElementId = 'output';
+
+updateElementWithText().then((text) => {
+  setTextToElement(outputElementId, text);
+});
